137ccd0e2ad3ad360886ff3552a8d504e779f588
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-05-27 13:46:56 +0200

    ok

--- src/components/pages/MoviesPage.jsx
+++ src/components/pages/MoviesPage.jsx
@@ -1,13 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+
+import { useState } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 
 function MoviesPage() {
+// получаем масив фильмов с сервера
+  const [movies, setMovies] = useState(
+    ["movies-id-1", "movies-id-2", "movies-id-3", "movies-id-4"]
+
+  );
+// searchParams - аткуальное значение из url , setSearchParams - меняет url. Добавлением обьекта
+  const [searchParams, setSearchParams] = useSearchParams()
+
+
+  // можно получить значение из url с помощью метода searchParams.get("a")) a - ключ, значение которого нам нужно получить послезнака вопроса
+  // http://localhost:3000/goit-react-hw-05-movies/movies?a=5&b=10
+  // searchParams.get("a") =>  5
+
+
+// console.log(searchParams.get("a")) 
+// console.log(setSearchParams)
+// по типу state, записываем значение movieId в const и доавляем как value input. Для сохранения значения введённого пользователем в input
+
+const movieId = searchParams.get("movieId")
+console.log(movieId)
+// useEffect(() => {
+
+
+//   return () => {
+//    http запрос
+//   };
+// }, [movieId])
+
+const updateQueryString = evt => {
+  setSearchParams({movieId: evt.target.value})
+}
+
+const visibleMovies = movies.filter( movie => movie.includes(movieId))
+
   return (
     <div>
-     {["movies-id-1", "movies-id-2", "movies-id-3", "movies-id-4"].map( movie => {
+
+      <input type="text" value={movieId || ''} onChange={updateQueryString} />
+
+      {/* <button onClick={()=> setSearchParams({c: "hello"})}>Change searchParams</button> */}
+      
+      <ul>
+      {/* {movies.map( movie => { */}
+     {visibleMovies.map( movie => {
       // `${movie}` чтобы прописать путь url и для строк и цифр для получаемых. Link - активная ссылку
-      return <Link key={movie} to={`${movie}`}>{movie}</Link>;
+      return (
+      <li key={movie}>
+      <Link  to={`${movie}`}>{movie}</Link>
+      </li>
+      );
       })}
+      </ul>
     </div>
   );
 };
